f4cdaa7127c4e4f7d58b4fa9b9af9ee80d8d9a39
Raul Ciobanu <rauliathos@gmail.com>
2019-01-09 21:39:49 +0000

    finished the MVP

---
+++ models/traveller.js
@@ -0,0 +1,36 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => journey.startLocation);
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => journey.endLocation);
+
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+
+  return this.journeys.filter((journey) => {
+   return journey.transport === transport;
+ });
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+  });
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
